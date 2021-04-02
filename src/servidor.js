781b29f1b7631c5a72a14d105f7748e49ad95a9c
Nailson Gomes <48133821+nailsongomesdk@users.noreply.github.com>
2021-04-02 13:16:49 -0300

    implementando serviço

--- src/servidor.js
+++ src/servidor.js
@@ -2,9 +2,23 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) =>{
-    res.send({nome: 'notebook', preco: 123.45})
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) =>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+
+    res.send(produto)
 })
 
 app.listen(porta, () => {
